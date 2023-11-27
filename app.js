7d28e3225bd94c991e0a44ada926e61c72078437
Paul <paulsk97@gmail.com>
2023-11-27 06:45:27 +0530

    Implement Level-4 security using salting and hashing with bcrypt npm package.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 //%%%%%%%%%
 require('dotenv').config();
 
+//But is bcrypt hashing still secure? Because the employees can still see your page if it's set so, if the employee gets your hash and salt. Am I right?
+
 const express = require("express");
 const bodyParser = require("body-parser"); 
 const mongoose = require("mongoose");
 
-//------//const encrypt = require("mongoose-encryption");  
-const md5 = require('md5');
+//------# const encrypt = require("mongoose-encryption");  
+//------* const md5 = require('md5');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -39,72 +44,66 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/login", function(req, res){
-  res.render("login");
-});
-
 app.get("/register", function(req, res){
   res.render("register");
 });
 
-//async function run(){
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+//async function run(){}
+//"await" keyword in JavaScript can only be used inside functions that are declared as "async".
 
 app.post("/register", async function(req, res){
-  const newUserModelObject = new userModelConstructorFunction({
-    email: req.body.username,
-    password: md5(req.body.password)     //Create hash using md5 npm package. Level 3 security.
-  });
-
-  /*
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    }else{
-      res.render("secrets");
+  //salted hashing.
+  bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+    const newUserModelObject = new userModelConstructorFunction({
+      email: req.body.username,
+      //password: md5(req.body.password)     //Create hash using md5 npm package. Level 3 security.
+      password: hash
+    });
+    
+    // Saving the details in the database and rendering the secrets page. Not compulsory necessory for a try-catch statement here.
+    try {
+      const savedUser = await newUserModelObject.save();     //"await" pauses async function, waits for promise resolution, and retrieves resolved value, enhancing asynchronous code readability and flow. ----// In JavaScript, a Promise is an object representing the eventual completion or failure of an asynchronous operation, and promise resolution refers to the point in time when the asynchronous operation represented by the Promise finishes successfully. When a Promise is created, it's in a pending state. It can either be resolved (fulfilled) with a value or rejected with a reason (an error). 
+      console.log(savedUser);    //print.
+      res.render("secrets");     //Yes here the secrets page comes after registration. Also, this being inside try-catch statement ensures that if the document is not properly saved inside the database, it is not going to show the secrets page.
+    } catch (error) {
+      console.error(error);
     }
-  })*/
-
-  try {
-    const savedUser = await newUserModelObject.save();
-    console.log(savedUser);  //print.
-    res.render("secrets");
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
+
+
+/*
+In the context of programming, "non-blocking" refers to the capability of a process or operation 
+to not halt the execution of other tasks while waiting for a particular operation to complete. In 
+asynchronous programming, non-blocking operations allow the program to continue executing other 
+tasks while waiting for potentially time-consuming operations (such as I/O operations or network 
+requests) to finish.
+*/
+
 
 app.post("/login", async function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);      //Create hash using md5 npm package. Level 3 security.
-
+  //------* const password = md5(req.body.password);      //Create hash using md5 npm package. Level 3 security.
   try{
     const foundUser = await userModelConstructorFunction.findOne({ email: username });
     if (foundUser){
-      if (foundUser.password===password){
-        res.render("secrets");
-        console.log("Success. Password is correct. Entered password : "+password +". Register password is : "+ foundUser.password);   //Here foundUser.password still gets printed as plain text here. This is still not secure.
-      }else{
-        console.log("Password is not correct. Entered password : "+password +". Register password is : "+ foundUser.password);   //Here foundUser.password still gets printed as plain text here. This is still not secure.
-      }
+      bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+        if (result==true){
+          console.log("Success. Password is correct.");  
+          res.render("secrets");            
+        }else{
+          console.log("Failure. Password is not correct.");
+        }
+      });
+
     }   
   }catch (error) {
     console.error(error);
   }
-
-  /*
-  // With the below code you get the following error : "Model.findOne() no longer accepts a callback."
-  userModelConstructorFunction.findOne({email:username},function(err, foundUser){
-    if (err){
-      console.log(err);
-    }else{
-      if (foundUser){
-        if (foundUser.password===password){
-          res.render("secrets");
-        }
-      }
-    }
-  });*/
-
 });
 
 app.listen(3000, function(){
