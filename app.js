534ca5995018f8c06c91e094c81b42dcae80f937
Paul Sebastian <iampaulsebastian@gmail.com>
2024-04-04 15:04:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ const passportLocalMongoose = require("passport-local-mongoose");  //This is wha
 const GoogleStrategy = require("passport-google-oauth20").Strategy;  //If you put it above this line it won't work according to Angela.
 const findOrCreate = require("mongoose-findorcreate");
 
-// --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));   //?
+app.set('view engine', 'ejs');   // For ejs.
+app.use(express.static("public"));  //?
 
 app.use(session({                                 // What are all these parameters and what do they do?
   secret: 'Our little secret - keyboard cat',
@@ -39,7 +39,7 @@ app.use(session({                                 // What are all these paramete
 app.use(passport.initialize());  // Initializes Passport middleware, enabling authentication setup and usage with various strategies in an Express app. After initialization, Passport can be configured with various authentication strategies such as LocalStrategy, OAuth, JWT, etc.
 app.use(passport.session());   // Initializes and utilizes session support for persistent login sessions using Passport.
 
-// --------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------
 
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewURLParser:true});
 
@@ -53,11 +53,11 @@ const userSchema= new mongoose.Schema({           //mongoose.Schema is a constru
 //userSchema.plugin(passportLocalMongoose);   //This is what will be used to hash and salt our passwords and save our users into our mongoDB database.
  
 userSchema.plugin(passportLocalMongoose, {
-  // Set usernameUnique to false to avoid a mongodb index on the username column!
+  // Set usernameUnique to false to avoid a mongodb index on the username column!   //?
   usernameUnique: false,
 
   findByUsername: function(model, queryParameters) {
-    // Add additional query parameter - AND condition - active: true
+    // Add additional query parameter - AND condition - active: true  //?
     queryParameters.active = true;
     return model.findOne(queryParameters);
   }
@@ -67,7 +67,7 @@ userSchema.plugin(findOrCreate);
 const userModelConstructorFunction = new mongoose.model("UserCollection", userSchema);    // Creating a model constructor function. "UserCollection" mentioned as value in brackets is the name of the collection.
 // A model is a constructor function that is used to create new documents based on the defined schema. It also provides methods for querying and interacting with the MongoDB collection.
 
-// ----------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------
 
 // syntax --> passport.use(strategy);      // All strategies have a name which, by convention, corresponds to the package name according to the pattern passport-{name}.
 passport.use(userModelConstructorFunction.createStrategy());    // ? // What is a strategy?  // This line of code is typically used when configuring Passport's authentication strategies, particularly with the LocalStrategy, in an Express.js application.
@@ -88,7 +88,7 @@ passport.deserializeUser(function(user, cb) {
   });
 });
 
-// ------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------
 
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
@@ -103,42 +103,50 @@ function(accessToken, refreshToken, profile, cb) {
 }
 ));
 
-// -----------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------
 
 app.get("/", function(req, res){
+  console.log('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz');
   res.render("home");
 });
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: ["profile"] }));
+  passport.authenticate('google', { scope: ["profile"] }),
+  function(){
+    console.log("gggggggggggggggggggggggggggggggggggggggggggg");   //Not printing.
+  });
 
 
 app.get('/auth/google/secrets', 
   passport.authenticate('google', { failureRedirect: '/login' }),     //Authenticate the user locally here.
   function(req, res) {
     // Successful authentication, redirect secrets.
+    console.log('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');
     res.redirect('/secrets');
   });
 
 app.get("/register", function(req, res){
+  console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
   res.render("register");
 });
 
 app.get("/login", function(req, res){
+  console.log('wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww');
   res.render("login");
 });
 
 app.get("/secrets", function(req, res){   // Should this be post ?
   if (req.isAuthenticated()){   // Here authentication does not just refer to checking if the password entered by the user is matching the password stored in the database, it also is ensuring the the password matches the one stored in cookies session and so on...
-    console.log("djikstra-1");
-    res.render("secrets");
+    console.log("djikstra-111111111111111111111");
+    res.render("secrets");      // If authenticated.
   }else{
-    console.log("djikstra-2");
+    console.log("djikstra-2222222222222222222222");
     res.redirect("/login");    // If not authenticated. 
   }
 });
 
 app.get('/logout', function(req, res, next){
+  console.log('uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu');
   req.logout(function(err) {
     if (err) { 
       console.log(err); 
@@ -148,9 +156,10 @@ app.get('/logout', function(req, res, next){
   });
 });
 
-// ------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
 
 app.post("/register", function(req, res){
+  console.log('kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk');
   userModelConstructorFunction.register({username:req.body.username, active: true}, req.body.password, function(err, user) {
     if (err) { 
       console.log(err);
@@ -164,22 +173,23 @@ app.post("/register", function(req, res){
 });
 
 app.post("/login", function(req, res){
+  console.log('mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm');
   const user = new userModelConstructorFunction({
     username : req.body.username,
     password : req.body.password
   });
   req.login(user, function(error) {
     if (error) { 
-      console.log(error);
+      console.log(error);    // Print if any error.
     }else {
-      passport.authenticate ("local")(req, res, function(){
+      passport.authenticate ("local")(req, res, function(){  // If authenticated, redirect to "/secrets" page/route.  "passport" npm package is used for using cookies and sessions.
         res.redirect("/secrets");
       });
     }
   });
 });
 
-// ------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
